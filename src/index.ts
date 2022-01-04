2f7dc1dc9bba004bafbf8629b176a13bc626fa1c
Sungjinyoung <sjy02161@naver.com>
2022-01-04 23:42:59 +0900

    todoList 생성 api 추가

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,67 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 
 const app = express()
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(bodyParser.json())
 
+enum TODO_STATUS_ENUM {
+  READY = 'ready',
+  IN_PROGRESS = 'inProgress',
+  DONE = 'done',
+}
+interface SubItemInterface {
+  description: string
+  done: boolean
+}
 
+interface TodoItemInterface {
+  id: number
+  title: string
+  description: string
+  deadline: number
+  status: TODO_STATUS_ENUM
+  subItems?: SubItemInterface
+}
 
-const todoList = [
-  { id: 0, title: "TodoList 제작하기", description: "TodoList 클라이언트와 서버를 개발해야함", deadline: new Date().getTime(), isCompleted: false }
+const todoList: Array<TodoItemInterface> = [
+  {
+    id: 0,
+    title: 'TodoList 제작하기',
+    description: 'TodoList 클라이언트와 서버를 개발해야함',
+    deadline: new Date('2022-01-30').getTime(),
+    status: TODO_STATUS_ENUM.READY,
+  },
 ]
 
-
 // 모든 todoList 받아오기
-app.get('/todolist/all', (req, res) => {
+app.get('//todolistall', (req, res) => {
   res.send(todoList)
 })
 
-//특정 id todoList 받아오기
+// 특정 id todoList 받아오기
 
-app.get('/todolist/:id', (req, res) => {
-  const todoItem = todoList.find(item => item.id === parseInt(req.params.id))
+//
+app.get('//todolistget/:id', (req, res) => {
+  const todoItem = todoList.find((item) => item.id === parseInt(req.params.id))
+  res.send(todoItem)
+})
+
+app.post('/todolist/create', (req, res) => {
+  const newTodoItem: TodoItemInterface = {
+    id: todoList.length,
+    title: req.body.title,
+    description: req.body.description,
+    deadline: req.body.deadline,
+    status: req.body.status,
+    subItems: req.body.subItems,
+  }
+  todoList.push(newTodoItem)
+  res.send(todoList)
 })
 
 // app.post('/todolist/create', (req, res) => {
@@ -45,6 +89,6 @@ app.get('/todolist/:id', (req, res) => {
 //   todoList.splice(index, 1)
 // })
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log('server is working now...')
 })
